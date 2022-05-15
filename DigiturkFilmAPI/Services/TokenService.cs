43bb71bf564d2ec334c9611ff751f6448fc9071c
burkayelbek <burkay.elbek@gmail.com>
2022-05-15 16:18:53 +0300

    Fixed hashng and salt encode + decode

--- DigiturkFilmAPI/Services/TokenService.cs
+++ DigiturkFilmAPI/Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using DigiturkFilmAPI.Domain;
 using DigiturkFilmAPI.Models;
 using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
@@ -11,6 +12,7 @@ namespace DigiturkFilmAPI.Services
     public class TokenService
     {
         private readonly IConfiguration _configuration;
+        private readonly string key = "57617b5d2349434b34734345635073433835777e2d244c31715535255a366773755a4d70532a5879793238235f707c4f7865753f3f446e633a21575643303f66";
         public TokenService(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -18,25 +20,33 @@ namespace DigiturkFilmAPI.Services
 
         public (string hash, string salt) CreatePasswordHash(string password)
         {
-            byte[] tempSalt = null;
-            byte[] tempHash = null;
+            //string tempSalt = null;
 
+            //using (var hmac = new HMACSHA256())
+            //{
+            //    var byteSalt = hmac.Key;
+            //    var byteHash = System.Text.Encoding.UTF8.GetBytes(password);
+            //    tempSalt = System.Text.Encoding.UTF8.GetString(byteSalt);
+            //}
 
-            using (var hmac = new HMACSHA256())
-            {
-                tempSalt = hmac.Key;
-                tempHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-            return (hash: Convert.ToBase64String(tempHash), salt: Convert.ToBase64String(tempSalt));
+            byte[] hash = HashHMAC(HexDecode(key), StringEncode(password));
+            string computedHash = HashEncode(hash);
+
+            return (computedHash, computedHash);
         }
 
         public bool VerifyPasswordHash(User user, string password)
         {
-            using (var hmac = new HMACSHA256(System.Text.Encoding.ASCII.GetBytes(user.PasswordSalt)))
-            {
-                var computedHash = Convert.ToBase64String(hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)));
-                return computedHash.SequenceEqual(user.PasswordHash);
-            }
+            byte[] hash = HashHMAC(HexDecode(key), StringEncode(password));
+            string computedHash = HashEncode(hash);
+            return computedHash.SequenceEqual(user.PasswordHash);
+
+            //using (var hmac = new HMACSHA256(System.Text.Encoding.UTF8.GetBytes(user.PasswordSalt)))
+            //{
+            //    var passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            //    var computedHash = System.Text.Encoding.UTF8.GetString(hmac.ComputeHash(passwordBytes));
+            //    return computedHash.SequenceEqual(user.PasswordHash);
+            //}
         }
 
         public string CreateToken(User user)
@@ -58,5 +68,46 @@ namespace DigiturkFilmAPI.Services
 
             return token;
         }
+
+        #region Hash Hex Functions
+        private static string HashHMACHex(string keyHex, string message)
+        {
+            byte[] hash = HashHMAC(HexDecode(keyHex), StringEncode(message));
+            return HashEncode(hash);
+        }
+
+        #endregion
+
+        #region Hash Functions
+        private static byte[] HashHMAC(byte[] key, byte[] message)
+        {
+            var hash = new HMACSHA256(key);
+            return hash.ComputeHash(message);
+        }
+
+        #endregion
+
+        #region Encoding Helpers
+        private static byte[] StringEncode(string text)
+        {
+            var encoding = new ASCIIEncoding();
+            return encoding.GetBytes(text);
+        }
+ 
+        private static string HashEncode(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+ 
+        private static byte[] HexDecode(string hex)
+        {
+            var bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = byte.Parse(hex.Substring(i * 2, 2), NumberStyles.HexNumber);
+            }
+            return bytes;
+        }
+        #endregion
     }
 }
